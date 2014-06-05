a024a053e7d73fe348fbc02907bb7b3b814e5394
Jacinda Zhong <jacindaz@gmail.com>
2014-06-05 14:20:40 -0400

    Setsup methods in each class, with pseudocode rought draft
     - creates reader methods to return recipe id, name, etc.
     - creates reader methods to return ingredient name, id, recipe_id
     - pseudocode: queries must return array of objects, instead of
       array of hashes
     - can store these arrays in the Recipe and Ingredient classes

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,24 @@
 class Ingredient
+
+  #query database
+  #return an array of Ingredient objects
+  #Ingredient object must contain:
+        # 1) ingredient name
+        # 2) recipe_id
+
+  #returns name of ingredient, to be accessed in Recipe class
+  def name
+    @name
+  end
+
+  #returns ingredient id
+  def id
+    @id
+  end
+
+  #returns recipe_id of that ingredient
+  def recipe_id
+    @recipe_id
+  end
+
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,4 +1,44 @@
 class Recipe
+
+  #query database
+  #return an array of Recipe objects
+  def initialize(id, name, description, instructions)
+    @id = id
+    @name = name
+    @description = description
+    @instructions = instructions
+
+  #return an array of Recipe objects
   def self.all
+    recipe_query = "SELECT id, name, description, instructions
+                    FROM recipes
+                    "
   end
+
+  #returns id of recipe
+  def id
+    @id
+  end
+
+  #returns name of recipe
+  def name
+    @name
+  end
+
+  #return recipe description
+  def description
+    @description
+  end
+
+  #return recipe instructions
+  def instructions
+    @instructions
+  end
+
+  #returns an array of Ingredient objects
+  def ingredients
+
+  end
+
+
 end
